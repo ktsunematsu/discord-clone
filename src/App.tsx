5bcd43bd7791a34fc4276548f265469e759dbfe7
Kazuki Tsunematsu <Kazuki.Tsunematsu@sre-group.co.jp>
2025-01-27 03:24:41 +0900

    ログイン機能を追加し、ユーザーが未ログインの場合にログインコンポーネントを表示するようにAppを更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import './App.scss';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
+import Login from './components/Login';
   
 function App() {
+  const user = null;
+
   return (
     <div className="App">
-      {/* Sidebar */}
-      <Sidebar />
-      {/* Chat */}
-      <Chat />
+      {user ? (
+        <>
+          {/* sidebar */}
+            {/* <Suspense fallback={<div>...Loading</div>}> */}
+            <Sidebar />
+            {/* </Suspense> */}
+
+          {/* home */}
+          <Chat />
+        </>
+      ) : (
+        <>
+          <Login />
+        </>
+      )}
     </div>
   );
 }
