a22119a3423625e947620e4dcbf947765502cc66
Kazuki Tsunematsu <Kazuki.Tsunematsu@sre-group.co.jp>
2025-02-03 23:54:28 +0900

    スタイルの改善：Appコンポーネントの幅を100vwに変更し、オーバーフローを隠す。ChatHeaderコンポーネントのスタイルをstyled-componentsに移行し、不要なSCSSファイルを削除。

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -1,40 +1,85 @@
+import styled from '@emotion/styled';
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import SearchIcon from "@mui/icons-material/Search";
-import SendIcon from "@mui/icons-material/Send";
 import HelpIcon from "@mui/icons-material/Help";
 import PushPinIcon from "@mui/icons-material/PushPin";
-import "./ChatHeader.scss";
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  border-bottom: 1px solid #ccc;
+  background-color: #2f3136;
+  width: 100%;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+  h3 {
+    color: white;
+    display: flex;
+    align-items: center;
+    .hash {
+      color: gray;
+      margin-right: 10px;
+      font-size: 24px;
+    }
+  }
+`;
+
+const HeaderRight = styled.div`
+  display: flex;
+  align-items: center;
+  color: #7b7c85;
+  gap: 15px;
+`;
+
+const SearchBar = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #40444b;
+  padding: 10px;
+  border-radius: 5px;
+  margin-right: 20px;
+  
+  input {
+    background: transparent;
+    outline: none;
+    color: white;
+    border: none;
+    margin-left: 5px;
+  }
+`;
 
 type Props = {
     channelName: string | null;
 };
 
-const ChatHeader = (props: Props) => {
-    const { channelName } = props;
-
+const ChatHeader = ({ channelName }: Props) => {
     return (
-        <div className="chatHeader">
-            <div className="chatHeaderLeft">
+        <Header>
+            <HeaderLeft>
                 <h3>
-                    <span className="chatHeaderHash">#</span>
+                    <span className="hash">#</span>
                     {channelName}
                 </h3>
-            </div>
+            </HeaderLeft>
 
-            <div className="chatHeaderRight">
+            <HeaderRight>
                 <NotificationsIcon />
                 <PushPinIcon />
                 <PeopleAltIcon />
-            <div className="chatHeaderSearch">
-                <input placeholder="Search" />
-                <SearchIcon />
-            </div>
-            <SendIcon />
-            <HelpIcon />
-            </div>
-        </div>
+                <SearchBar>
+                    <input placeholder="Search" />
+                    <SearchIcon />
+                </SearchBar>
+                <HelpIcon />
+            </HeaderRight>
+        </Header>
     );
-}
+};
 
 export default ChatHeader;
